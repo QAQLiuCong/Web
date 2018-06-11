9ac835f8ae42e4047b81ae76047ea93e09ba7c54
QAQliucong <794418142@qq.com>
2018-06-11 15:39:52 +0800

    修改

---
+++ src/App.vue
@@ -0,0 +1,24 @@
+<template>
+  <div id="app">
+    <img src="./assets/logo.png">
+    <p>父{{text}}</p>
+    父<input type="text" v-model="text">
+    <HelloWorld :word.sync="text"><HelloWorld/>
+  </div>
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld.vue';
+import './assets/scss/common.scss';
+export default {
+    data () {
+        return {
+            text:''
+        }
+    },
+    components: {
+        HelloWorld
+    },
+}
+</script>
+
---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,35 @@
+<template>
+  <div class="hello">
+    <h1>{{ msg }}</h1>
+    <p>子{{ word }}</p>
+    子<input type="text" v-model="str">
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld',
+  data () {
+    return {
+      msg: 'Welcome to Your Vue.js App',
+      str: '',
+    }
+  },
+  props: {
+    word: ''
+  },
+  watch: {
+    str: function(newValue, oldValue) {
+      //每当str的值改变则发送事件update:word , 并且把值传过去
+      this.$emit('update:word', newValue)
+    }
+  }
+}
+</script>
+
+<style>
+h1, h2 {
+  font-weight: normal;
+  text-align: center;
+}
+</style>
